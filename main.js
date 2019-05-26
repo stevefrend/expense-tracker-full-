689e1f72c9d8c6f47b7e752c3a60efeb1bc8249b
Steve Frend <stevefrend1@gmail.com>
2019-05-26 08:53:54 -0700

    radio button option, single button
    after making a function to add new category, will try a cleaner interface with just a button that leads to a new html page. then just choosing category with radio buttons every time you do an entry

--- main.js
+++ main.js
@@ -1,63 +1,81 @@
 
-const allCategories = {
-    groceries: [],
-    gas: []
-};
-
-var modal = document.getElementById("simpleModal");
-var modalBtn = document.getElementById("modalBtn")
-var closeBtn = document.getElementById("closeBtn");
-
-modalBtn.addEventListener('click', openModal);
-function openModal() {
-    modal.style.display = 'block';
-}
-
-closeBtn.addEventListener('click', closeModal);
-function closeModal() {
-    modal.style.display = 'none';
-}
-
+const allCategories = [];
 
 // Variables and event listeners
 
-function groceriesFunction () {
-    let groceriesSpent;
 
-    const form = document.querySelector('#groceries');
-    const amountInput = document.getElementById('groceries-amount');
-    const commentInput = document.getElementById('groceries-comment');
+
+
+
+const mainDiv = document.getElementById('categories');
+const newCategoryForm = document.querySelector('#new-category-form');
+const category = document.getElementById('category');
+newCategoryForm.addEventListener('submit', addNewCategoryToPage);
+
+function addNewCategoryToPage(e) {
+    e.preventDefault();
+    let categoryName = category.value.toLowerCase();    
+    function createNewHTML (categoryName) {
+        let newDiv = document.createElement('div');
+        let newForm = document.createElement('form');
+        let newLabel = document.createElement('label');
+        let newInputNumber = document.createElement('input');
+        let newInputComment = document.createElement('input');
+        let newButtonEnter = document.createElement('button');
+
+        newDiv.id = categoryName + '-div';
+        newDiv.className = 'category-div'
+        newForm.id = categoryName + '-form';
+
+        newLabel.htmlFor = categoryName;
+        newLabel.textContent = categoryName;
+
+        newInputNumber.id = categoryName + '-number';
+        newInputNumber.type = 'number';
+        newInputNumber.required = true;
+        newInputNumber.placeholder = 'amount';
+
+        
+        newInputComment.id = categoryName + '-comment';
+        newInputComment.type = 'text';
+        newInputComment.placeholder = 'comment';
+        
+
+        newButtonEnter.id = categoryName + '-enter'
+        newButtonEnter.type = 'submit';
+        newButtonEnter.textContent = 'Enter'
+        
+        newDiv.appendChild(newForm);
+        newForm.appendChild(newLabel);
+        newForm.appendChild(newInputNumber);
+        newForm.appendChild(newInputComment);
+        newForm.appendChild(newButtonEnter);
+        
+        mainDiv.appendChild(newDiv);
+        newCategoryForm.reset();   
+        
+    }
+    createNewHTML(categoryName);
+
+    // CREATE EVENT LISTENER FOR NEW SUBMIT AND ADD TO ARRAY, ALSO PREVENT DEFAULT
+    const form = document.getElementById(categoryName + '-form');
+    const amountInput = document.getElementById(categoryName + '-number');
+    const commentInput = document.getElementById(categoryName + '-comment');
     
-    form.addEventListener('submit', runEvent);
-    
-    function runEvent(e) {
+    form.addEventListener('submit', addToArray);
+    function addToArray (e) {
         e.preventDefault();
         let inputValue = Number(amountInput.value);
-        addItem(inputValue, commentInput.value);
-        console.table(allCategories.groceries);
-        getTotal();
-        console.log(`Total spent: ${groceriesSpent}`);
-        document.getElementById("groceries").reset();
-        alert(`$${inputValue} added to Groceries`)
-        modal.style.display = 'none';
-        
-    }
-    const addItem = (newAmount, newComment) => allCategories.groceries.push({amount: newAmount, comment:newComment, date: new Date().toDateString()});
-
-
-    const getTotal = function () {
-        groceriesSpent = allCategories.groceries.reduce(function (total, entry) {
-            return total + entry.amount;
-        }, 0);
-        
+        let textValue = commentInput.value;
+        allCategories.push({category: categoryName, amount: inputValue, comment: textValue, date: new Date().toDateString()});
+        console.table(allCategories)
     }
     
     
+
 }
 
-
-groceriesFunction();
-
+ 
 
 
 
@@ -66,7 +84,7 @@ groceriesFunction();
 
 
 
-
+//1. make a function to add a new catogory in html, then make a netural function for adding to a category. will need to pull ID from category somehow
 
 
 // - make a functions that: adds the html elements to the page with appendChild
