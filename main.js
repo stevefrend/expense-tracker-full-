be46dcf443eaefcf9cc596a4b66f99c96a20da22
Steve Frend <stevefrend1@gmail.com>
2019-05-28 07:41:51 -0700

    changed radio to dropdown
    fully working with dropdown. add summary, and average options from here.

--- main.js
+++ main.js
@@ -8,8 +8,10 @@ const newCategory = document.getElementById('category');
 
 const newCategoryForm = document.querySelector('#new-category-form');
 const newExpenseForm = document.querySelector('#new-expense-form');
+
 const amountInput = document.getElementById('amount-input');
 const commentInput = document.getElementById('comment-input');
+const categoryDropdown = document.getElementById('dropdown');
 
 
 newCategoryForm.addEventListener('submit', addNewCategoryToPage);
@@ -18,62 +20,34 @@ newExpenseForm.addEventListener('submit', addExpense);
 function addNewCategoryToPage(e) {
     e.preventDefault();
     let categoryName = newCategory.value.toLowerCase(); 
-    
-       
-    
-    function createNewHTML (categoryName) {
-        let newCategoryRadio = document.createElement('input');
-        let newCategoryLabel = document.createElement('label');
-        
-        newCategoryRadio.id = categoryName + '-radio';
-        newCategoryRadio.type = 'radio';
-        newCategoryRadio.value = categoryName;
-        newCategoryRadio.name = 'CategoryName'
-        newCategoryRadio.className = 'categories-class';
-        newCategoryRadio.required = true;
+    const newCategoryOption = document.createElement('option');
 
-        newCategoryLabel.for = categoryName;
-        newCategoryLabel.textContent = categoryName;
-        
-        newExpenseForm.appendChild(newCategoryRadio);
-        newExpenseForm.appendChild(newCategoryLabel);
-        
-        newCategoryForm.reset();   
-        
-    }
-    createNewHTML(categoryName);
+    newCategoryOption.id = categoryName;
+    newCategoryOption.value = categoryName;
+    newCategoryOption.className = 'categories-class';
+    newCategoryOption.text = categoryName;
+    
+    categoryDropdown.appendChild(newCategoryOption);
+
+    newCategoryForm.reset();   
+    
     allCategories.push(categoryName);  
       
 }
 
 function addExpense (e) {
     e.preventDefault();
-    let categoryName = newCategory.value.toLowerCase(); 
     let inputValue = Number(amountInput.value);
     let textValue = commentInput.value;
+    let selectedCategory = categoryDropdown.value;
 
-    // make variable with loop that returns the name of the selected radio
+    allExpenses.push({category: selectedCategory, amount: inputValue, comment: textValue, date: new Date().toDateString()});
 
-    allExpenses.push({category: , amount: inputValue, comment: textValue, date: new Date().toDateString()});
+    newExpenseForm.reset(); 
+
     console.log(allExpenses);
 
 }
-
-
-// // CREATE EVENT LISTENER FOR NEW SUBMIT AND ADD TO ARRAY, ALSO PREVENT DEFAULT
-// const form = document.getElementById(categoryName + '-form');
-// const amountInput = document.getElementById(categoryName + '-number');
-// const commentInput = document.getElementById(categoryName + '-comment');
-
-// form.addEventListener('submit', addToArray);
-
-// function addToArray (e) {
-//     e.preventDefault();
-//     let inputValue = Number(amountInput.value);
-//     let textValue = commentInput.value;
-//     allExpenses.push({category: categoryName, amount: inputValue, comment: textValue, date: new Date().toDateString()});
-//     console.table(allExpenses)
-// }
 
 
 
